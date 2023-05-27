59c1942fc7355b861d4522927a42112f452b44d6
Prannaya Gupta <50899509+ThePyProgrammer@users.noreply.github.com>
2023-05-27 21:10:52 +0800

    clean code :)

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,9 @@ import requests
 import datetime
 import tensorflow as tf
 
-from clean_text import clean_texts
+import git, os
 
+from clean_text import clean_texts
 
 from pathlib import Path
 CWD = Path(__file__).parent.resolve()
@@ -22,21 +23,10 @@ convos = []
 def home():
     return render_template("chat.html")
 
-@app.route("/chat")
-def chat():
-    return render_template("chat.html")
-
 @app.route("/cyberbully")
 def cyberbully():
     messages = clean_texts([request.args.get("msg")], tokenizer)
     return jsonify(dict(score = model.predict(messages).T[0].tolist()[0]))
-    
-time = lambda: datetime.datetime.now().strftime("%H:%M")
-
-@app.route("/getUserHtml")
-def getUser():
-    userText = request.args.get('msg')
-    return {"user": f"<div class='container darker'><p class='user-msg'>{userText}</p><span class='time-right'>{time()}</span></div>"}
 
 @app.route("/get")
 def get():
@@ -44,12 +34,8 @@ def get():
     botText = requests.post("https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill", headers={"Authorization": "Bearer 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"}, json={"inputs": {"past_user_inputs": [i[0] for i in convos], "generated_responses": [i[1] for i in convos], "text": userText}, "parameters": {"repetition_penalty": 1.33}}).json()["generated_text"]
     convos.append((userText, botText))
     return {"bot": botText.strip()}
-    # return {"user": f"<div class='container darker'><span class='user-msg'>{userText}</span><br><span class='time-right'>{time()}</span></div>", "bot": f"<div class='container'><span>{botText}</span><br><span class='time-left'>{time()}</span></div>"}
-
 
 # Github webhooking
-import git, os
-
 @app.route('/update_server', methods=['POST'])
 def webhook():
     repo = git.Repo(CWD)
