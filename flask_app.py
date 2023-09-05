e0b7b0c58ba44e74731e7b5c2f1779153c9875b7
Haloen <72333894+Haloen@users.noreply.github.com>
2023-09-05 11:13:57 +0800

    No more GitHub, its too cancer

--- flask_app.py
+++ flask_app.py
@@ -74,46 +74,6 @@ def get():
     # return {"user": f"<div class='container darker'><span class='user-msg'>{userText}</span><br><span class='time-right'>{time()}</span></div>", "bot": f"<div class='container'><span>{botText}</span><br><span class='time-left'>{time()}</span></div>"}
 
 
-############################################
-#                                          #
-#              Set Up Github               #
-#                                          #
-############################################
-
-# Github webhooking
-import git, os
-
-import hmac
-import hashlib
-
-# Function to check that it is from github
-def is_valid_signature(x_hub_signature, data, private_key):
-    # x_hub_signature and data are from the webhook payload
-    # private key is your webhook secret
-    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
-    algorithm = hashlib.__dict__.get(hash_algorithm)
-    encoded_key = bytes(private_key, 'latin-1')
-    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
-    return hmac.compare_digest(mac.hexdigest(), github_signature)
-
-
-@app.route('/update_server', methods=['POST'])
-def webhook():
-    # Github request checker
-    x_hub_signature = request.headers.get('X-Hub-Signature')
-    if not is_valid_signature(x_hub_signature, request.data, "secret"):
-        return "YOU ARE NOT GITHUB!!"
-
-    repo = git.Repo(CWD)
-    origin = repo.remotes.origin
-    origin.pull()
-    return 'Updated PythonAnywhere successfully', 200
-
-############################################
-#                                          #
-#            Set Up Github END             #
-#                                          #
-############################################
 
 if __name__ == "__main__": 
     app.run(debug=True)
